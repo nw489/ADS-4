4d8556f07d1c7b320cfc233ebad05093a6f98448
Letavin Egor <letavin123@yandex.ru>
2026-03-29 18:49:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -14,35 +14,11 @@ int countPairs1(int *arr, int len, int value) {
 
 int countPairs2(int *arr, int len, int value) {
     int count = 0;
-    int left = 0;
-    int right = len - 1;
-
-    while (left < right) {
-        int sum = arr[left] + arr[right];
-        if (sum == value) {
-            if (arr[left] == arr[right]) {
-                int n = right - left + 1;
-                count += n * (n - 1) / 2;
-                break;
-            } else {
-                int leftVal = arr[left];
-                int rightVal = arr[right];
-                int leftCount = 0;
-                int rightCount = 0;
-                while (left <= right && arr[left] == leftVal) {
-                    leftCount++;
-                    left++;
-                }
-                while (left <= right && arr[right] == rightVal) {
-                    rightCount++;
-                    right--;
-                }
-                count += leftCount * rightCount;
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value) {
+                count++;
             }
-        } else if (sum < value) {
-            left++;
-        } else {
-            right--;
         }
     }
     return count;
@@ -65,15 +41,10 @@ int binarySearch(int *arr, int left, int right, int target) {
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len; i++) {
-        int target = value - arr[i];
-        int first = binarySearch(arr, i + 1, len - 1, target);
-        if (first != -1) {
-            int last = first;
-            while (last + 1 < len && arr[last + 1] == target) {
-                last++;
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value) {
+                count++;
             }
-            count += (last - first + 1);
-            i = last;
         }
     }
     return count;
